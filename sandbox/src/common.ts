49cd4faba6b23b731ab2a56706333a36e803cdcf
Stephen Sang <nguyentuanquangsang1999@gmail.com>
2025-03-02 22:40:01 +0700

    latest update with pending status

--- sandbox/src/common.ts
+++
@@ -1,5 +0,0 @@
-export function bufferToBigInt(buffer: Buffer): bigint {
-	const bufferHex = buffer.toString('hex');
-	// Seems stupid to have string as an intermediate step
-	return BigInt('0x' + bufferHex);
-}
